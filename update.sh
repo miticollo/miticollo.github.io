5d0edd88758f3d86c6da5cc97126e541c19a9b4d
Lorenzo <lorenzo_ferron@hotmail.it>
2024-10-13 20:10:50 +0200

    Add support for RootHide packages

--- update.sh
+++ update.sh
@@ -12,12 +12,25 @@ gzip -c9 Packages > Packages.gz
 zstd -c19 Packages > Packages.zst
 
 apt-ftparchive contents ./debs > Contents-iphoneos-arm
+apt-ftparchive contents ./debs > Contents-iphoneos-arm64e
+
 bzip2 -c9 Contents-iphoneos-arm > Contents-iphoneos-arm.bz2
+bzip2 -c9 Contents-iphoneos-arm64e > Contents-iphoneos-arm64e.bz2
+
 xz -c9 Contents-iphoneos-arm > Contents-iphoneos-arm.xz
+xz -c9 Contents-iphoneos-arm64e > Contents-iphoneos-arm64e.xz
+
 xz -5fkev --format=lzma Contents-iphoneos-arm > Contents-iphoneos-arm.lzma
+xz -5fkev --format=lzma Contents-iphoneos-arm64e > Contents-iphoneos-arm64e.lzma
+
 lz4 -c9 Contents-iphoneos-arm > Contents-iphoneos-arm.lz4
+lz4 -c9 Contents-iphoneos-arm64e > Contents-iphoneos-arm64e.lz4
+
 gzip -c9 Contents-iphoneos-arm > Contents-iphoneos-arm.gz
+gzip -c9 Contents-iphoneos-arm64e > Contents-iphoneos-arm64e.gz
+
 zstd -c19 Contents-iphoneos-arm > Contents-iphoneos-arm.zst
+zstd -c19 Contents-iphoneos-arm64e > Contents-iphoneos-arm64e.zst
 
 if [ "${1}" = "my" ]; then
   apt-ftparchive release -c ../config/my.conf . > Release
@@ -29,4 +42,3 @@ echo "${2}" | gpg --batch --yes --pinentry-mode=loopback --passphrase-fd 0 -abs 
 echo "${2}" | gpg --batch --yes --pinentry-mode=loopback --passphrase-fd 0 --clearsign -u 896CC9E708C847210A2293F2997233590E26A3A2 -o InRelease Release
 
 cd -
-
